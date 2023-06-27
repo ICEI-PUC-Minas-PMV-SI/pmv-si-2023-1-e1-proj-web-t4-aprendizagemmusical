7d692b30f9fd855d849c33aab7cccf42b0c23457
Marília R. de A <marilia.andrade.1457572@sga.pucminas.br>
2023-06-27 01:40:28 -0300

    envio #3 com finalização da geração dinâmica de cards em theory.html

--- src/scripts/cardsGenerator.js
+++ src/scripts/cardsGenerator.js
@@ -1,21 +1,30 @@
 const divAlter = document.querySelector("#elemento-dinamico");
 
-const urlImagens = [{imagem:'drum_percussion.jpg'}, {imagem:'trumpet.jpg'},{imagem:'violin.jpg'}, {imagem:"guitar.jpg"}, {imagem:"keys.jpg"}, {imagem:"jazz.jpeg"}]
+const urlImagens = [
+  { image: "music_sheet.jpg", textH2: "title-course-1", textP: "courseLessonsAndTime-1", textAlt: "Photo of music sheets" },
+  { image: "music_history.jpeg", textH2: "title-course-2", textP: "courseLessonsAndTime-2", textAlt: "Photo of an Egyptian mural with a drawing of people with ancient instruments" },
+  { image: 'drum_percussion.jpg', textH2: "title-course-3", textP: "courseLessonsAndTime-3", textAlt: "Photo of a drum" },
+  { image: "jazz.jpeg", textH2: "title-course-4", textP: "courseLessonsAndTime-4", textAlt: "Photo of jazz band" },
+  { image: 'violin.jpg', textH2: "title-course-5", textP: "courseLessonsAndTime-5", textAlt: "Photo of a violin" },
+  { image: 'trumpet.jpg', textH2: "title-course-6", textP: "courseLessonsAndTime-6", textAlt: "Photo of trumpet player" },
+  { image: "keys.jpg", textH2: "title-course-7", textP: "courseLessonsAndTime-7", textAlt: "Photo of music sheets" },
+  { image: "guitar.jpg", textH2: "title-course-8", textP: "courseLessonsAndTime-8", textAlt: "Photo of acoustic guitar player" },
+];
 
-function cardsCourse(nomeImagem){
-    const linhaDiv = `<div class="course-info-box">
+function cardsCourse(cardRef) {
+  const linhaDiv = `<div class="course-info-box">
     <figure>
-      <img src="../assets/images/${nomeImagem.imagem}" alt="Photo of an Egyptian mural with a drawing of people with ancient '			instruments">
+      <img src="../assets/images/${cardRef.image}" alt="${cardRef.textAlt}">
     </figure>
-    <h2 data-text-node="title-course-2"></h2>
-    <p data-text-node="courseLessonsAndTime-2"></p>
-    </div>`
+    <h2 data-text-node="${cardRef.textH2}"></h2>
+    <p data-text-node="${cardRef.textP}"></p>
+  </div>`;
 
-    divAlter.insertAdjacentHTML("afterbegin", linhaDiv)
+  divAlter.insertAdjacentHTML("afterbegin", linhaDiv);
 }
 
-function carregarCards(listaImages){
-    listaImages.map((imagem) => cardsCourse(imagem))
+function carregarCards(listaImages) {
+  listaImages.map((imagem) => cardsCourse(imagem));
 }
 
-carregarCards(urlImagens)
+carregarCards(urlImagens);
