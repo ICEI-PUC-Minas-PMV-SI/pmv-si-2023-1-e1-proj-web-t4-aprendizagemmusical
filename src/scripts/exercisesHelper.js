de9b028a160c399c63c94b34bc5ddcfbe759a166
nathalialsb <lsb.nathalia@gmail.com>
2023-06-30 14:57:08 -0300

    new rhythm exercise screen~

--- src/scripts/exercisesHelper.js
+++ src/scripts/exercisesHelper.js
@@ -24,7 +24,7 @@ function redirectToPercepcaoMusical() {
   }
 
 function redirectToPercepcaoRitmica() {
-    window.location.href = 'exercisesChooseMode.html';
+    window.location.href = 'exercisesRhythm.html';
   }
 
 function redirectToCompleteEscala() {
